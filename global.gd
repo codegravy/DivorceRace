fed47ace09e684243e1ec3481e79de4a39bfc870
Blixa Morgan <blixa@projectmakeit.com>
2019-01-26 17:50:03 -0600

    add netcode stuffs

--- global.gd
+++ global.gd
@@ -7,12 +7,25 @@ extends Node
 var player = load("res://elements/player/player.tscn").instance()
 var bag = load("res://elements/player/playerBag.tscn").instance()
 var hand = load("res://elements/player/playerHand.tscn").instance()
+var players = Node.new();
 var vr = false
 var peer = null
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
+	var global = get_node("/root/global")
+	var root = get_node("/root/")
+	var vr = ARVRServer.find_interface("OpenVR")
+	players.set_name("players")
+	root.call_deferred("add_child",players)
+	if vr and vr.initialize():
+		get_viewport().arvr = true
+		get_viewport().hdr = false
+		OS.vsync_enabled = false
+		Engine.target_fps = 90
+		global.vr = true
+		players.add_child(global.player)
 	setupNetwork()
 	var id = str(peer.get_unique_id())
 	player.set_name(id)
@@ -24,6 +37,9 @@ func setupNetwork():
 	get_tree().set_network_peer(peer)
 	get_tree().set_meta("network_peer",peer)
 
+	
+func ready():
+	rpc_id(1, "player_ready", str(peer.get_unique_id()))
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
