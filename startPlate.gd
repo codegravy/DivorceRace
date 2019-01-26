6e3126b6653dfeac4c819fe0f557b04c96d63280
Blixa Morgan <blixa@projectmakeit.com>
2019-01-26 11:53:36 -0600

    add start room stuffs, add player vr scene

--- startPlate.gd
+++ startPlate.gd
@@ -1,15 +1,8 @@
-extends Node
-
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+extends RigidBody
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	pass;
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _on_RigidBody_body_entered(body):
+	print('player ready');
+	pass # replace with function body
