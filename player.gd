67d2eb1c92d5d76e7303f14da662227cfcae3623
Blixa Morgan <blixa@projectmakeit.com>
2019-01-26 18:31:48 -0600

    cleanup testroom curfufel

--- player.gd
+++ player.gd
@@ -3,15 +3,16 @@ extends Spatial
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+var global
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
+	global = get_node("/root/global");
 	pass;
 	
 func setHand(setBag):
 	var currentRightHand = get_node("VROrigin/rightHand").get_child(0);
-	var global = get_node("/root/global");
 	print(currentRightHand)
 	get_node("VROrigin/rightHand").remove_child(currentRightHand);
 	var newHand
@@ -20,7 +21,18 @@ func setHand(setBag):
 	else:
 		newHand = global.hand
 	get_node("VROrigin/rightHand").add_child(newHand)
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	var positions = {}
+	positions.base = get_node(".").global_transform
+	positions.camera = get_node("VROrigin/PlayerCamera").global_transform
+	positions.left = get_node("VROrigin/leftHand").global_transform
+	positions.right = get_node("VROrigin/rightHand").global_transform
+	print(positions)
+	rpc_unreliable("updatePlayerPositions",str(global.peer.get_unique_id()),positions)
+	pass
+#remote func updatePlayerPositions(id,positions):
+#	var player = get_node("/root/players/").get_node(id)
+#	player.global_transform = positions.base
+#	get_node("VROrigin/PlayerCamera").global_transform = positions.camera
+#	get_node("VROrigin/leftHand").global_transform = positions.left
+#	get_node("VROrigin/rightHand").global_transform = positions.right
